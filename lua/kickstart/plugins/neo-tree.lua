d8b73b61fd20c0e98b6bfcca18b97de8db457bd6
AlexBringh <115021574+AlexBringh@users.noreply.github.com>
2026-06-10 23:49:09 +0200

    Removed unnecesary files and comments in the nvim config. More cleanup later.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -4,11 +4,6 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
